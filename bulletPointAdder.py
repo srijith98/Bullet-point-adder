cce8630a524f25c4ad6f93247009e0a5ba2f08e4
Srijith <srijiths1998@gmail.com>
2019-07-08 16:19:45 +0530

    Added options for numbering and bullet points.

--- bulletPointAdder.py
+++ bulletPointAdder.py
@@ -1,17 +1,27 @@
 import pyperclip
 
-text=pyperclip.paste()
+print('Copy the lines which need bullet points to your clipboard.')
+print('Enter 1 if done.')
+flag=int(input())
+if flag==1:
+    text=pyperclip.paste()
 
-if text=='':
-    print('Copy lines of texts which need bullet points to the clipboard before calling the program.')
-    exit()
+    print('Enter 1 for numbering and 2 for bullet points:')
+    bulltype=int(input())
+    lines=text.split('\n')
+    if bulltype==1:
+        for i in range(len(lines)-1):
+            lines[i]=str(i+1)+'. '+lines[i]
+    elif bulltype==2:
+        for i in range(len(lines)-1):
+            lines[i]='* '+lines[i]
+    else:
+        print('Enter 1 or 2 only.')
+        exit()
+    text = '\n'.join(lines)
 
-lines=text.split('\n')
-for i in range(len(lines)):
-    lines[i]='* '+lines[i]
+    pyperclip.copy(text)
+    print("The bulleted lines are copied to the clipboard.")
 
-text = '\n'.join(lines)
 
-pyperclip.copy(text)
-print("The bulleted lines are copied to the clipboard.")
 
